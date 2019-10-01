1683e1f0e737e84fe79abae152ffcc2d4b8c72f8
darts <darts@tcd.ie>
2019-10-01 16:44:43 +0100

    Update hw1.pl

--- hw1.pl
+++ hw1.pl
@@ -11,5 +11,11 @@ add2(X+Y, s(P+Q), Z) :- add2(X+Y, 0, A), add2(P+Q, s(0), B), add(A, B, Z).
 add2(X+Y, P, Z) :- add(X,Y,A), add(A, P, Z).
 add2(P, X+Y, Z) :- add2(X+Y, P, Z).
 
-add2(p(s(X)), Y, Z) :- add2(X, Y, Z).
+add2((p(s(X))), Y, Z) :- add2(X, Y, Z).
+add2((s(p(X))), Y, Z) :- add2(X, Y, Z).
+add2(X, s(p(Y)), Z) :- add2(Y, X, Z).
+add2(X, p(s(Y)), Z) :- add2(Y, X, Z).
+add2(X, Y, Z) :- add(X, Y, Z).
 
+add2(p(X)+s(Y), P, Z) :- add2(X+Y, P, Z).
+add2(P, p(X)+s(Y), Z) :- add2(P, X+Y, Z).
