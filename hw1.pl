c6f4d2a4a1bb53dd3bcbb3b6c80c04d6f9103c37
darts <darts@tcd.ie>
2019-10-10 14:26:32 +0100

    Done AFAIK

--- hw1.pl
+++ hw1.pl
@@ -9,13 +9,13 @@ eval(X-Y, Z) :- subtract(X, Y, Z). %ex6
 add2(0, Y, Z) :- eval(Y, Z).
 add2(p(s(X)), Y, Z) :- add2(X, Y, Z).
 add2(s(p(X)), Y, Z) :- add2(X, Y, Z).
-add2(p(X), s(Y), Z) :- add2(X, Y, Z).
 add2(s(X), p(Y), Z) :- add2(X, Y, Z).
+add2(p(X), s(Y), Z) :- add2(X, Y, Z).
 add2(s(X), Y, s(Z)) :- add2(X, Y, Z).
 add2(p(X), Y, p(Z)) :- add2(X, Y, Z).
-add2(X+Y, B, Z) :- add2(X, Y, A), add2(A, B, Z).
 add2(-X, Y, Z) :- minus(X, A), add2(A, Y, Z).
 add2(X, -Y, Z) :- minus(Y, A), add2(X, A, Z).
+add2(X+Y, B, Z) :- add2(X, Y, A), add2(A, B, Z).
 add2(X-Y, A, Z) :- subtract(X, Y, B), add2(B,A,Z). %ex6
 
 minus(0,0).
