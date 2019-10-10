17440f06737db16f2411f24bcf90f7d6d2dcfb0b
darts <darts@tcd.ie>
2019-10-10 11:33:08 +0100

    Ex 5 passes

--- hw1a2.pl
+++ hw1a2.pl
@@ -4,6 +4,8 @@ eval(s(p(X)), Y) :- eval(X,Y).
 eval(s(X), s(Y)) :- eval(X,Y).
 eval(p(X), p(Y)) :- eval(X,Y).
 eval(X+Y, Z) :- add2(X, Y, Z).
+eval(X-Y, Z) :- subtract(X, Y, Z). %ex6
+
 
 
 add2(0, Y, Z) :- eval(Y, Z).
@@ -16,11 +18,17 @@ add2(p(X), s(Y), Z) :- add2(X, Y, Z).
 add2(X+Y, B, Z) :- add2(X, Y, A), add2(A, B, Z).
 add2(-X, Y, Z) :- minus(X, A), add2(A, Y, Z).
 add2(X, -Y, Z) :- minus(Y, A), add2(X, A, Z).
+add2(X-Y, A, Z) :- subtract(X, Y, B), add2(B,A,Z). %ex6
 
 
 minus(0,0).
+minus(-X, X).
 minus(X+Y, Z) :- add2(X,Y,A), minus(A, Z).
 minus(s(p(X)), Z) :- minus(X,Z).
 minus(p(s(X)), Z) :- minus(X,Z).
 minus(s(X), p(Y)) :- minus(X,Y).
 minus(p(X), s(Y)) :- minus(X,Y).
+
+subtract(X, Y, Z) :- minus(Y, A), add2(X, A, Z). %ex5
+
+
